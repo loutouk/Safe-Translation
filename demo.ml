8e4f8567ad0d2aa8daf113c5153083bf90a1b9ce
E17B270A <louis.boursier@etu.univ-nantes.fr>
2020-04-26 16:05:30 +0200

    update demo.ml

---
+++ demo.ml
@@ -0,0 +1,36 @@
+exception Error ;;
+
+let stack = ref [] ;;
+
+let id = ref 0 ;;
+
+let generateId () = let x = !id in id := !id + 1 ; x ;;
+
+let pushId localId = stack := localId::!stack ;;
+
+let popId () =
+   begin match !stack with
+   | [] -> failwith "Error, empty stack!"
+   | hd::tl -> stack := tl
+   end
+   
+let checkId localId =
+   begin match !stack with
+   | [] -> failwith "Error, stack id mismatch!"
+   | hd::_ -> if hd != localId then failwith "Error, stack id mismatch!"
+   end
+
+let test_vulnerable f =
+   let x = ref 0 in x := 0; f (); x := 1; f (); !x;;
+
+let test_translated f = let x = ref 0 in let id1 = 
+   generateId () in pushId id1 ; x := 0; f () ; checkId id1; x := 1; f () ; checkId id1; !x ; popId () ;;
+
+let opposant () =
+   let c = ref 0 in
+   test_translated (fun _ -> if !c = 0
+                           then c := !c + 1
+                           else  try begin test_translated(fun _ -> raise (Error)); () end
+                                 with Error -> ());;
+
+opposant ();;
